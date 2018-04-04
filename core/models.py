08f172b7b8753138b3e6217b9a823a0c550225c7
Nisha Pant <nisha007pant@gmail.com>
2018-04-03 19:18:21 -0500

    updated images

--- core/models.py
+++ core/models.py
@@ -30,6 +30,7 @@ class Question(models.Model):
     year = models.CharField(max_length=250)
     created_date = models.DateTimeField(
             default=timezone.now)
+    is_complete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.topic + " " + self.year
